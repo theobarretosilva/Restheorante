7bfd8053225cba469ac94f9cc9ef1e2de4ad21d2
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-13 12:25:25 -0300

    Atualizando projeto

--- src/components/Secoes/Secoes.jsx
+++ src/components/Secoes/Secoes.jsx
@@ -1,10 +1,51 @@
-import { func } from "prop-types";
 import { Card } from "../Card/Card";
 import "./Secoes.css";
 import secoes from "../../secoes.json";
 import PropTypes from 'prop-types'
 
-export const Secoes = ({entradas, saladas, pratosPrincipais, sobremesas, bebidas}) => {
+const subSecoes = ["Carnes", "Massas"];
+
+// const percorreSub = () => {
+//   subSecoes.length > 0 ? (subSecoes.map((item, index) => {
+//     return <ul><li key={index}><h3>{item}</h3></li></ul>
+//   })) : Secoes
+// }
+
+function filtrar(){
+  const secao = document.getElementsByClassName('nomeSecao').value;
+  if(secao == "entradas"){
+    {secoes.entradas.map((item, index) => {
+      return <Card card={item} key={index} />;
+    })}
+  } else if(secao == "saladas"){
+    {secoes.saladas.map((item, index) => {
+      return <Card card={item} key={index} />;
+    })} 
+  } else if(secao == "pratosPrincipais"){
+    {secoes.pratosPrincipais.map((item, index) => {
+      return <Card card={item} key={index} />;
+    })}
+  } else if(secao == "sobremesas"){
+    {secoes.sobremesas.map((item, index) => {
+      return <Card card={item} key={index} />;
+    })}
+  } else if(secao == bebidas){
+    {secoes.bebidas.map((item, index) => {
+      return <Card card={item} key={index} />;
+    })}
+  }
+}
+
+function percorreSub2(){
+  if(subSecoes.length > 0){
+    subSecoes.map((item, index) => {
+      return <ul><li key={index}><h3>{item}</h3></li></ul>
+    })
+    const teste = secoes.filter(filtrar());
+  }
+}
+
+export const Secoes = ({entradas, saladas, pratosPrincipais, sobremesas, bebidas, bSecoes}) => {
 
   return (
     <div className="divGeralSecoes">
@@ -12,19 +53,34 @@ export const Secoes = ({entradas, saladas, pratosPrincipais, sobremesas, bebidas
         <div id="div1"></div>
         <div className="secoes">
           <div>
-            <a href=""><h2 className="nomeSecao">{entradas}</h2></a>
+            <a onClick={percorreSub2()}><h2 value="entradas" className="nomeSecao">{entradas}</h2></a>
+            <ul>
+              <li>{bSecoes}</li>
+            </ul>
           </div>
           <div>
-            <a href=""><h2 className="nomeSecao">{saladas}</h2></a>
+            <a onClick={percorreSub2()}><h2 value="saladas" className="nomeSecao">{saladas}</h2></a>
+            <ul>
+              <li>{bSecoes}</li>
+            </ul>
           </div>
           <div>
-            <a href=""><h2 className="nomeSecao">{pratosPrincipais}</h2></a>
+            <a onClick={percorreSub2()}><h2 value="pratosPrincipais" className="nomeSecao">{pratosPrincipais}</h2></a>
+            <ul>
+              <li>{bSecoes}</li>
+            </ul>
           </div>
           <div>
-            <a href=""><h2 className="nomeSecao">{sobremesas}</h2></a>
+            <a onClick={percorreSub2()}><h2 value="sobremesas" className="nomeSecao">{sobremesas}</h2></a>
+            <ul>
+              <li>{bSecoes}</li>
+            </ul>
           </div>
           <div>
-            <a href=""><h2 id="nomeSecao">{bebidas}</h2></a>
+            <a onClick={percorreSub2()}><h2 value="bebidas" className="nomeSecao" id="nomeSecao">{bebidas}</h2></a>
+            <ul>
+              <li>{bSecoes}</li>
+            </ul>
           </div>
         </div>
         <div id="div2"></div>
@@ -59,6 +115,7 @@ Secoes.propTypes = {
     saladas: PropTypes.string.isRequired,
     pratosPrincipais: PropTypes.string.isRequired,
     sobremesas: PropTypes.string.isRequired,
-    bebidas: PropTypes.string.isRequired
+    bebidas: PropTypes.string.isRequired,
+    subSecoes: PropTypes.array
   })
 }
