f4ddf6d268723a0ead3c7b72f90bd9c24516ce67
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-16 18:32:30 -0300

    Atualizando projeto

--- src/components/Secoes/Secoes.jsx
+++ src/components/Secoes/Secoes.jsx
@@ -7,71 +7,52 @@ import PropTypes from 'prop-types'
 const subSecoesPP = ["Carnes vermelhas", "Aves", "Peixes e frutos do mar", "Massas", "Risotos"];
 const subSecoesB = ["Sem àlcool", "Vinhos", "Drinks", "Cervejas"];
 
-function calma(){
-  subSecoesPP.map((item, index) => {
-    return <SubSecoes subSecao={item} key={index} />;
-  })
-}
-
-function percorreSubSecoesB(){
-  if(subSecoesB.length > 0){
-    subSecoesB.map((item, index) => {
-      return <h3 key={index}>{item}</h3>
-    })
-  }
-}
-
 export const Secoes = ({entradas, saladas, pratosPrincipais, sobremesas, bebidas}) => {
   return (
-    <div className="divGeralSecoes">
-      <div className="divSecoes">
-        <div id="div1"></div>
-        <div className="secoes">
-          <div>
-            <h2 value="entradas" className="nomeSecao">{entradas}</h2>
-          </div>
-          <div>
-            <h2 value="saladas" className="nomeSecao">{saladas}</h2>
-          </div>
-          <div>
-            <h2  id="pratosPrincipais" value="pratosPrincipais" className="nomeSecao">{pratosPrincipais}</h2>
-          </div>
-          <div>
-            <h2 value="sobremesas" className="nomeSecao">{sobremesas}</h2>
-          </div>
-          <div>
-            <h2 value="bebidas" id="nomeSecao">{bebidas}</h2>
-          </div>
-        </div>
-        <div id="div2"></div>
-      </div>
-
-      <div className="secoesECards">
-        <div id="divSubSecoes" className="divSubSecoes">
-          {subSecoesPP.map((item, index) => {
-            return <SubSecoes subSecao={item} key={index} />;
-          })}
-        </div>
- 
-        <div className="secoesCards">
+    <div className="secoesCards">
+      <div>
+        <h2 className="nomeSecao">{entradas}</h2>
+        {subSecoesPP.map((item, index) => {
+          return <SubSecoes subSecoes={item} key={index} />;
+        })}
+        <div className="cards">
           {secoes.entradas.map((item, index) => {
             return <Card card={item} key={index} />;
           })}
+        </div>
+      </div>
+      <div className="divCards">
+        <h2 className="nomeSecao">{saladas}</h2>
+        <div className="cards">
           {secoes.saladas.map((item, index) => {
             return <Card card={item} key={index} />;
           })}
+        </div>
+      </div>
+      <div className="divCards">
+        <h2 className="nomeSecao">{pratosPrincipais}</h2>
+        <div className="cards">
           {secoes.pratosPrincipais.map((item, index) => {
             return <Card card={item} key={index} />;
           })}
+        </div>
+      </div>
+      <div className="divCards">
+        <h2 className="nomeSecao">{sobremesas}</h2>
+        <div className="cards">
           {secoes.sobremesas.map((item, index) => {
             return <Card card={item} key={index} />;
           })}
+        </div>
+      </div>
+      <div className="divCards">
+        <h2 className="nomeSecao">{bebidas}</h2>
+        <div className="cards">
           {secoes.bebidas.map((item, index) => {
             return <Card card={item} key={index} />;
           })}
         </div>
       </div>
-      
     </div>
   );
 };
--- src/components/SubSecoes/SubSecoes.jsx
+++ src/components/SubSecoes/SubSecoes.jsx
@@ -1,27 +1,9 @@
 import './SubSecoes.css'
+
 import PropTypes from 'prop-types'
-import secoes from '../../secoes.json'
-import { Card } from '../Card/Card';
 
-export function SubSecoes ({subSecao}) {
-    const subSecoesPP = ["Carnes vermelhas", "Aves", "Peixes e frutos do mar", "Massas", "Risotos"];
-
-    function filtrar(secao){
-        if(secoes.pratosPrincipais[secao] === "carnesVermehas"){
-            return secao;
-        }
-    }
-
-    const filtrado = secoes.pratosPrincipais.filter(filtrar);
-    if(subSecoesPP.length > 0){
-        console.log(filtrado)
-        return(
-            <h3 className='subSecoes'>{subSecao}</h3>
-        )
-    }else{
-        return;
-    }
-    
+export const SubSecoes = ({subSecoes: {subSecao}}) => {
+    return <h3 className='subSecoes'>{subSecao}</h3>
 }
 
 SubSecoes.propTypes = {
