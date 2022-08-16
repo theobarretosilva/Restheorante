5880cb97ee0bcc8c3fdfd1cf4c8ad62f9e7d1780
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-16 16:58:23 -0300

    Atualizando projeto

--- src/components/SubSecoes/SubSecoes.jsx
+++ src/components/SubSecoes/SubSecoes.jsx
@@ -5,10 +5,21 @@ import { Card } from '../Card/Card';
 
 export function SubSecoes ({subSecao}) {
     const subSecoesPP = ["Carnes vermelhas", "Aves", "Peixes e frutos do mar", "Massas", "Risotos"];
+
+    function filtrar(secao){
+        if(secoes.pratosPrincipais[secao] === "carnesVermehas"){
+            return secao;
+        }
+    }
+
+    const filtrado = secoes.pratosPrincipais.filter(filtrar);
     if(subSecoesPP.length > 0){
+        console.log(filtrado)
         return(
             <h3 className='subSecoes'>{subSecao}</h3>
         )
+    }else{
+        return;
     }
     
 }
